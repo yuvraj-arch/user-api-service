6e4447a415dd59564fc5242f4fd8d72002f73392
Neelam <neelam@Yuvrajs-MacBook-Pro.local>
2026-02-10 00:02:10 +0530

    Update code

--- src/main/java/com/api/user_api_service/UserApiServiceApplication.java
+++ src/main/java/com/api/user_api_service/UserApiServiceApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserApiServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UserApiServiceApplication.class, args);
 	}
-
+//  Main file
 }
